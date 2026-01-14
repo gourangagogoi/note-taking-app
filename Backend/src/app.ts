821ad6997e5051e301c3ed1c5200198dd1b695c2
gourangagogoi <gourangagogoi01@gmail.com>
2026-01-14 20:45:52 +0530

    minor fixes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { userRoutes } from "./routes/user.routes";
-import { errorHandler } from "./routes/errorHandler";
+import { errorHandler } from "./utils/errorHandler";
 export const app = express();
 
 app.use(express.json());
-app.use("/user", userRoutes);
 app.use(errorHandler);
+app.use("/user", userRoutes);
